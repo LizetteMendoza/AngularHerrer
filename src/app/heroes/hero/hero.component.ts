3643357e069f0aea3c7bb8995e1359f10a2e0613
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2023-08-31 19:02:43 -0600

    creacion de componetes heroe y list

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
