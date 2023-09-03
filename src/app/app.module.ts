76baa8443d97952c2b69f2bc90992310ca62edfe
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2023-09-02 18:45:13 -0600

    creacion de modulos para counter y heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
