6c4f055e125ef1175ec3940162812e43bbab37ac
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2023-09-04 12:45:59 -0600

    se crearon componentes list y add-character y se configuraron en dbz.module, ademas se traslado el docigo den man-page al html de cada componente.

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css']
+})
+export class AddCharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
