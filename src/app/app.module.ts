941e630bc510c1e10e1383b06d71631bc09e1947
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2023-09-03 21:50:11 -0600

    creacion y configuracion de modulo DBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/components/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
 
 import { AppComponent } from './app.component';
 
@@ -14,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
